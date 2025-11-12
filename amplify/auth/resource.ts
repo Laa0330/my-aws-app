60bd1b46f32f0cc872273dd3c6aea9ba2362c2a1
Laila Alkhatib <lailaalkhatib@my.unt.edu>
2025-11-12 10:41:35 -0600

    Fixed TypeScript Error

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineFunction } from '@aws-amplify/backend';
 export const postConfirmation = defineFunction({
   name: 'post-confirmation',
 });
